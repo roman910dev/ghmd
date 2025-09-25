c5b10b2cdf5948063a2edb9a74e15660af044bcf
Román Via-Dufresne Saus <roman910dev@gmail.com>
2025-09-25 13:29:41 +0200

    feat: :sparkles: error when an invalid option is used

--- src/ghmd/__main__.py
+++ src/ghmd/__main__.py
@@ -6,80 +6,85 @@ import requests
 
 
 def main():
-    files = [argv for argv in sys.argv[1:] if not argv.startswith('--')]
+    files = [argv for argv in sys.argv[1:] if not argv.startswith("--")]
+    options = [argv for argv in sys.argv[1:] if argv.startswith("--")]
 
-    theme = (
-        '-dark' if '--dark' in sys.argv else
-        '-light' if '--light' in sys.argv else
-        ''
-    )
+    theme = ""
+    embed_css = False
+    mode = "gfm"
 
-    embed_css = '--embed-css' in sys.argv
+    for option in options:
+        if option == "--dark":
+            theme = "-dark"
+        elif option == "--light":
+            theme = "-light"
+        elif option == "--embed-css":
+            embed_css = True
+        elif option == "--no-gfm":
+            mode = "markdown"
+        else:
+            raise Exception(f"Invalid option: {option}")
 
-    mode = 'markdown' if '--no-gfm' in sys.argv else 'gfm'
-
-    if any(file.endswith('.html') for file in files):
+    if any(file.endswith(".html") for file in files):
         raise Exception(
-            'File cannot have .html extension because it would be overwritten')
+            "File cannot have .html extension because it would be overwritten"
+        )
 
     css_uri = (
-        'https://cdnjs.cloudflare.com/ajax/libs/'
-        f'github-markdown-css/5.8.1/github-markdown{theme}.min.css'
+        "https://cdnjs.cloudflare.com/ajax/libs/"
+        f"github-markdown-css/5.8.1/github-markdown{theme}.min.css"
     )
 
     if embed_css:
         res = requests.get(css_uri)
         if res.status_code != 200:
             raise Exception(
-                'Could not get css. '
-                'Check your internet connection or try without --embed-css.'
+                "Could not get css. "
+                "Check your internet connection or try without --embed-css."
             )
-        css = f'<style>{res.text}</style>'
+        css = f"<style>{res.text}</style>"
     else:
         css = (
-            '<link '
+            "<link "
             'rel="stylesheet" '
             f'href="{css_uri}" '
             'crossorigin="anonymous" '
             'referrerpolicy="no-referrer" '
-            '/>'
+            "/>"
         )
 
-    headers = {'Accept': 'application/vnd.github+json'}
-    if github_token := os.environ.get('GITHUB_TOKEN'):
-        headers['Authorization'] = f'Bearer {github_token}'
+    headers = {"Accept": "application/vnd.github+json"}
+    if github_token := os.environ.get("GITHUB_TOKEN"):
+        headers["Authorization"] = f"Bearer {github_token}"
 
     for file in files:
-        file_content = open(file, 'r', encoding="utf-8").read()
+        file_content = open(file, "r", encoding="utf-8").read()
 
-        titleSearch = re.search(r'^# (.*)$', file_content, re.MULTILINE)
-        title = titleSearch.group(1) if titleSearch else ''
+        titleSearch = re.search(r"^# (.*)$", file_content, re.MULTILINE)
+        title = titleSearch.group(1) if titleSearch else ""
 
         dirname = os.path.dirname(__file__)
-        template = open(os.path.join(dirname, 'md-template.html'), 'r').read()
+        template = open(os.path.join(dirname, "md-template.html"), "r").read()
 
-        res = requests.post('https://api.github.com/markdown',
-                            headers=headers,
-                            data=json.dumps({
-                                'text': file_content,
-                                'mode': mode
-                            }))
+        res = requests.post(
+            "https://api.github.com/markdown",
+            headers=headers,
+            data=json.dumps({"text": file_content, "mode": mode}),
+        )
 
         if res.status_code != 200:
             raise Exception(
-                'Could not convert markdown to HTML. '
-                'Check your internet connection.'
+                "Could not convert markdown to HTML. Check your internet connection."
             )
 
-        filename = '.'.join(file.split('.')[:-1])
-        with open(f'{filename}.html', 'w+', encoding='utf-8') as f:
+        filename = ".".join(file.split(".")[:-1])
+        with open(f"{filename}.html", "w+", encoding="utf-8") as f:
             f.write(
-                template
-                .replace('{{ .CSS }}', css)
-                .replace('{{ .Title }}', title)
-                .replace('{{ .Content }}', res.text)
+                template.replace("{{ .CSS }}", css)
+                .replace("{{ .Title }}", title)
+                .replace("{{ .Content }}", res.text)
             )
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
