29dd446dfb439293c32de55936f05686cc495c23
Román Via-Dufresne Saus <roman910dev@gmail.com>
2026-03-03 15:33:25 +0100

    style(browser): :art: format files

--- src/browser/src/main.jsx
+++ src/browser/src/main.jsx
@@ -4,7 +4,7 @@ import { App } from './App'
 import './styles.css'
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+	<StrictMode>
+		<App />
+	</StrictMode>,
 )
