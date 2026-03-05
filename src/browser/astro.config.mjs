d8a5502cf6ee605b1a151d9fc2455f17f6754021
Román Via-Dufresne Saus <56510981+roman910dev@users.noreply.github.com>
2026-03-05 12:32:51 +0100

    Add sitemap and robots support for browser site

--- src/browser/astro.config.mjs
+++ src/browser/astro.config.mjs
@@ -1,6 +1,8 @@
 import { defineConfig } from 'astro/config'
 import react from '@astrojs/react'
+import sitemap from '@astrojs/sitemap'
 
 export default defineConfig({
-  integrations: [react()],
+  site: 'https://ghmd.roman910.dev',
+  integrations: [react(), sitemap()],
 })
